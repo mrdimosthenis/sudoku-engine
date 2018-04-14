2c99430d8ee6a5e44787aa2d98771739550e9dbc
mrdimosthenis <mrdimosthenis@hotmail.com>
2018-04-14 20:22:26 +0300

    parse and solve

--- src/Parse.hs
+++ src/Parse.hs
@@ -1,34 +1,31 @@
 module Parse where
 
-import Data.List (intercalate)
+import Data.List (partition, intercalate)
 import Data.Char (isSpace, isDigit)
 import Traverse
-
-isValidSize :: Int -> Bool
-isValidSize n = (n > 1) && (squareRoot * squareRoot == n)
-    where squareRoot = intSqrt n
-
-hasValidIntElems :: Int -> [Int]  -> Bool
-hasValidIntElems size intElems = all (`elem` [1..size]) intElems
 
 trim :: String -> String
 trim = f . f
    where f = reverse . dropWhile isSpace
 
---isValidString :: String -> Bool
---isValidString str = (isValidSize size)
---                 && (all (all isDigit) $ nonAsteriskElems)
---                 && (hasValidIntElems size $ map (\x -> read x :: Int) nonAsteriskElems)
---    where   wrds = words $ trim str
---            size = length wrds
---            nonAsteriskElems = filter (\s -> s /= "*") wrds
+groupN :: Int -> [Int] -> [[Int]]
+groupN _ [] = []
+groupN n l
+  | n > 0 = (take n l) : (groupN n (drop n l))
+  | otherwise = error "Negative n"
 
-isValidString :: String -> Either String Puzzle
-isValidString str
-    | isValidSize size =
-        Left ("You provided " ++ show size ++ " elements. Valid number of elements: 16, 81, 144 ...")
-    | (all (all isDigit) $ nonAsteriskElems) && (hasValidIntElems size $ map (\x -> read x :: Int) nonAsteriskElems) =
-        Left ("There are invalid elements. Valid elements: " ++ "*, " ++ (intercalate ", " $ map show [1..size]))
+parseAndSolve :: String -> Either String (Maybe Puzzle)
+parseAndSolve str
+    | (elemsNum <= 1) || (size * size /= elemsNum) =
+        Left ("You provided " ++ show elemsNum ++ " elements. Valid number of elements: 16, 81, 144 ...")
+    | not $ null nonValidElems =
+        Left ("Invalid elements: " ++ intercalate ", " nonValidElems)
+    | otherwise = Right (solve
+                       . (:[])
+                       . groupN (intSqrt elemsNum)
+                       . map (\x -> if x == "*" then 0 else read x)
+                       $ wrds)
     where   wrds = words $ trim str
-            size = length wrds
-            nonAsteriskElems = filter (\s -> s /= "*") wrds
+            elemsNum = length wrds
+            size = intSqrt elemsNum
+            (_, nonValidElems) = partition (\wrd -> wrd == "*" || (all isDigit wrd && read wrd `elem` [1..size])) wrds
